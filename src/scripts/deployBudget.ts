2d21d70d1b8daf1ef602400b3298c33c5190aa33
Quazia <alf40k@gmail.com>
2024-12-20 13:50:36 -0500

    fix(scripts): tweak config and account handling in budget address

--- src/scripts/deployBudget.ts
+++ src/scripts/deployBudget.ts
@@ -1,14 +1,15 @@
-import { BoostCore } from '@boostxyz/sdk'
+import { BoostCore, ManagedBudget } from '@boostxyz/sdk'
 import { BoostRegistry } from '@boostxyz/sdk'
 import { Roles } from '@boostxyz/sdk'
 import { config } from "./config"
-
-const core = new BoostCore({ config })
+import { Account, Address, } from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
 const registry = new BoostRegistry({ config })
 const account = {
     privateKey: process.env.PRIVATE_KEY as `0x${string}`,
     address: process.env.WALLET_ADDRESS as `0x${string}`,
   }
+  const core = new BoostCore({ config, account: privateKeyToAccount(account.privateKey) })
 
 async function main() {
   try {
@@ -23,10 +24,13 @@ async function main() {
         roles: [Roles.ADMIN, Roles.MANAGER],
       })
     )*/
-    const budget = await core.ManagedBudget({
-        owner: account.address,
-        authorized: [account.address, core.assertValidAddress()],
-        roles: [Roles.ADMIN, Roles.MANAGER],
+    const budget = new ManagedBudget({
+    config,
+    account: privateKeyToAccount(account.privateKey),
+    }, {
+      owner: account.address,
+      authorized: [account.address, core.assertValidAddress()],
+      roles: [Roles.ADMIN, Roles.MANAGER],
     });
 
     const budgetAddress = budget.assertValidAddress()
